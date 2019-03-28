d45d0400ae76a92357b6e3d1ad85b73c478410f1
jtferson <jtferson@gmail.com>
2019-03-28 15:16:01 +0300

    Create ISceneType.cs

---
+++ Scripts/ISceneType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jtfer.Ecp.Unity
+{
+    public interface ISceneType
+    {
+        string Name { get; }
+    }
+}
